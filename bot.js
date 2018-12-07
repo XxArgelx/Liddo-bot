42637d8d5df9bd471f58fd3a88a114de5a9b7f6b
XxArgelx <45679280+XxArgelx@users.noreply.github.com>
2018-12-07 01:22:57 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ fs.readdir('./commands/', (err, files) => {
   log(`Loading a total of ${files.length} commands.`);
   files.forEach(f => {
     let props = require(`./commands/${f}`);
-    log(`Loading Command: ${props.help.name}. ðŸ‘Œ`);
+    log(`Loading Command: ${props.help.name}. 👌`);
     client.commands.set(props.help.name, props);
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
