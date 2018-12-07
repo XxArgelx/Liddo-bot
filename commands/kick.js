cd48186819711509b69f0a4a68f8e7b15f408bfa
XxArgelx <45679280+XxArgelx@users.noreply.github.com>
2018-12-07 16:41:20 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,31 +1,33 @@
-const Discord = require('discord.js');
-exports.run = (client, message, args) => {
-  let reason = args.slice(1).join(' ');
-  let user = message.mentions.users.first();
-  let modlog = client.channels.find('name', 'mod-log');
+const {RichEmbed} = require('discord.js');
+const {caseNumber} = require('../util/caseNumber.js');
+const {parseUser} = require('../util/parseUser.js');
+const settings = require('../settings.json');
+exports.run = async (client, message, args) => {
+  const user = message.mentions.users.first();
+  parseUser(message, user);
+  const modlog = client.channels.find('name', 'mod-log');
+  const caseNum = await caseNumber(client, modlog);
   if (!modlog) return message.reply('I cannot find a mod-log channel');
-  if (reason.length < 1) return message.reply('You must supply a reason for the kick.');
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to kick them.').catch(console.error);
 
-  if (!message.guild.member(user).kickable) return message.reply('I cannot kick that member');
-  message.guild.member(user).kick();
+  // message.guild.member(user).kick();
 
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setDescription(`**Action:** Kick\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
+  const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
+  const embed = new RichEmbed()
+  .setColor(0x00AE86)
+  .setTimestamp()
+  .setDescription(`**Action:** Kick\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
+  .setFooter(`Case ${caseNum}`);
   return client.channels.get(modlog.id).send({embed});
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
   aliases: [],
-  permLevel: 0
+  permLevel: 2
 };
 
 exports.help = {
   name: 'kick',
-  description: 'Echa al usuario mencionado.',
-  usage: 'kick [@usuario] [razón]'
+  description: 'Kicks the mentioned user.',
+  usage: 'kick [mention] [reason]'
 };
