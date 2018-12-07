9ece2309aa61ae69a6296e911719a1301b95638b
XxArgelx <45679280+XxArgelx@users.noreply.github.com>
2018-12-07 02:29:52 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -14,6 +14,6 @@ exports.conf = {
 
 exports.help = {
   name: 'ping',
-  description: 'Ping/Pong command. I wonder what this does? /sarcasm',
+  description: 'Muestra la velocidad de tu conexión con el servidor',
   usage: 'ping'
 };
