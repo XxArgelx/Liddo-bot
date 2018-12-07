eeeb3474a09b756eaff9f24012f1894913f32cf2
XxArgelx <45679280+XxArgelx@users.noreply.github.com>
2018-12-07 02:39:20 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 exports.run = (client, message) => {
   message.channel.send('Ping?')
     .then(msg => {
-      msg.edit(`Pong! (took: ${msg.createdTimestamp - message.createdTimestamp}ms)`);
+      msg.edit(`Pong! (Tomó: ${msg.createdTimestamp - message.createdTimestamp}ms)`);
     });
 };
 
